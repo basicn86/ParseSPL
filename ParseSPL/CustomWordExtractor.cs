4b270924f2dac3af20f467d285d2d1aa6ef93f8a
Nedim Basic <basicn86@gmail.com>
2023-10-28 18:15:35 -0400

    Added Custom Word Extractor

---
+++ ParseSPL/CustomWordExtractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content
+
+namespace ParseSPL
+{
+    static class CustomWordExtractor
+    {
+        public static IEnumerable<Word> GetWords(Page page)
+        {
+
+        }
+    }
+}
--- ParseSPL/Program.cs
+++ ParseSPL/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ParseSPL
+﻿
+
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace ParseSPL
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //if args are empty, then return
+            if (args.Length == 0) return;
+
+            PdfDocument doc = PdfDocument.Open(args[0]);
+            Page page = doc.GetPage(1); //assume the first page is the SPL
+            IEnumerable<Word> words = CustomWordExtractor.GetWords(page);
         }
     }
 }
