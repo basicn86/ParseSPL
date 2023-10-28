69a18fef2120f42179398048a0295ae101b99cf5
Nedim Basic <basicn86@gmail.com>
2023-10-28 19:07:42 -0400

    Added get date function

--- ParseSPL/CustomBlockExtractor.cs
+++ ParseSPL/CustomBlockExtractor.cs
@@ -16,27 +16,32 @@ namespace ParseSPL
         //Returns a list of words, filtered by their font size
         public static IEnumerable<IEnumerable<Word>> GetWords(Page page)
         {
+            //Filter words by font size
             var fontSizes = page.Letters.GroupBy(x => Math.Round(x.FontSize)).Distinct();
             var words = new List<Word>();
-
 
             foreach (var fontSize in fontSizes)
             {
                 var letters = page.Letters.Where(x => Math.Round(x.FontSize) == fontSize.Key).ToList();
 
+                //return one set of words, one at a time
                 yield return NearestNeighbourWordExtractor.Instance.GetWords(letters);
             }
         }
 
+        //Gets a set of text blocks, separated by font size
         public static IEnumerable<TextBlock> GetBlocks(Page page)
         {
             IEnumerable<TextBlock> blocks = new List<TextBlock>();
 
+            //iterate through each set of words, filtered by their font size
             foreach(IEnumerable<Word> words in GetWords(page))
             {
+                //concatenate the blocks
                 blocks = blocks.Concat(RecursiveXYCut.Instance.GetBlocks(words));
             }
 
+            //finally, return the blocks
             return blocks;
         }
     }
--- ParseSPL/Program.cs
+++ ParseSPL/Program.cs
@@ -1,12 +1,34 @@
-﻿
-
+﻿using System.Text.RegularExpressions;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis;
 
 namespace ParseSPL
 {
     internal class Program
     {
+        public static DateTime GetDateFromBlocks(IEnumerable<TextBlock> blocks)
+        {
+            //iterate through each block
+            foreach (var block in blocks)
+            {
+                Regex dateRegex = new Regex(@"\d{1,2}[\/\- ]\d{1,2}[\/\- ]\d{2,4}");
+                Match match = dateRegex.Match(block.Text);
+
+                if (match.Success)
+                {
+                    //try to parse the datetime, on fail: continue to the next iteration
+                    if (DateTime.TryParse(match.Value, out DateTime date))
+                    {
+                        return date;
+                    }
+                }
+            }
+
+            //if no date is found, throw an exception that the date was not found
+            throw new Exception("Date not found");
+        }
+
         static void Main(string[] args)
         {
             //if args are empty, then return
@@ -25,6 +47,18 @@ namespace ParseSPL
                 Console.WriteLine("BLOCK: " + i++);
                 Console.WriteLine(block.Text);
                 Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //try to get the date from the blocks
+            try
+            {
+                DateTime date = GetDateFromBlocks(blocks);
+                Console.WriteLine("Date: " + date);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
         }
     }
